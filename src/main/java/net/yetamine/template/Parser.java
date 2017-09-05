7109b593ee88489496883f2835ac7f7aefa915b5
Petr Doležal <petr.dolezal@matfyz.cz>
2017-09-05 23:34:13 +0200

    Extend Parser with iteration support

--- src/main/java/net/yetamine/template/Parser.java
+++ src/main/java/net/yetamine/template/Parser.java
@@ -16,6 +16,14 @@
 
 package net.yetamine.template;
 
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+import java.util.Spliterators;
+import java.util.function.Consumer;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
 /**
  * Encapsulates a parsing strategy and the state during parsing a template.
  *
@@ -65,4 +73,83 @@ public interface Parser<R> {
      * @return the input to parse
      */
     String input();
+
+    /**
+     * Returns a stream coupled with this parser.
+     *
+     * <p>
+     * The stream may throw {@link TemplateSyntaxException} if could not parse
+     * the template.
+     *
+     * @return a stream coupled with this parser
+     */
+    default Stream<R> stream() {
+        return StreamSupport.stream(Spliterators.spliteratorUnknownSize(new ParsingIterator<>(this), 0), false);
+    }
+
+    /**
+     * Passes all results of {@link #next()} until {@link #done()} to the given
+     * consumer.
+     *
+     * <p>
+     * This method is a shortcut for {@code stream().forEachOrdered(consumer)}
+     * and the default implementation should be more efficient.
+     *
+     * @param consumer
+     *            the consumer to invoke. It must not be {@code null}.
+     *
+     * @throws TemplateSyntaxException
+     *             if the parser fails to parse the template
+     */
+    default void forEach(Consumer<? super R> consumer) {
+        new ParsingIterator<>(this).forEachRemaining(consumer);
+    }
+}
+
+/**
+ * Default {@link Iterator} adapter for {@link Parser}.
+ *
+ * <p>
+ * This class is not public intentionally, because {@link Parser} is not meant
+ * as an iterator, although it has similar interface. There is a difference in
+ * perceiving {@link Parser#done()} and {@link Parser#next()} semantics. On the
+ * other hand, it makes sense to turn the parser into a stream of its results.
+ * The {@link Iterator} adaptation exists merely for adapting to {@link Stream}
+ * as there is no better way in Java 8.
+ *
+ * @param <R>
+ *            the type of the result
+ */
+final class ParsingIterator<R> implements Iterator<R> {
+
+    /** Source parser. */
+    private final Parser<? extends R> parser;
+
+    /**
+     * Creates a new instance.
+     *
+     * @param source
+     *            the source parser. It must not be {@code null}.
+     */
+    public ParsingIterator(Parser<? extends R> source) {
+        parser = Objects.requireNonNull(source);
+    }
+
+    /**
+     * @see java.util.Iterator#hasNext()
+     */
+    public boolean hasNext() {
+        return !parser.done();
+    }
+
+    /**
+     * @see java.util.Iterator#next()
+     */
+    public R next() {
+        if (parser.done()) {
+            throw new NoSuchElementException();
+        }
+
+        return parser.next();
+    }
 }
